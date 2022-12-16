ef01942bcfab975396439e23a37576ed9e190069
Rafael Márquez <ing17romc@gmail.com>
2022-12-16 14:34:27 -0600

    updated layout

--- src/components/controls/Layaout/index.js
+++ src/components/controls/Layaout/index.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types'
 
 const Layaout = ({ leftOptions, rightOptions, footer, children, getCurrentPath, currentPath }) => {
 	return <div>
-		<div className='main-container bg-white'>
+		<div className='page-container bg-white'>
 			<NavBar
 				leftOptions={leftOptions}
 				rightOptions={rightOptions}
